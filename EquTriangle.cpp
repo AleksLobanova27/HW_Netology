b3dd6b562712bc398263057746995d156d7a6b6b
AleksLobanova27 <elaginas27@gmail.com>
2025-07-09 15:49:22 +0300

    Delete EquTriangle.cpp

--- EquTriangle.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "EquTriangle.h"
-
-using namespace std;
-
-EquTriangle::EquTriangle(double a)
-    : Triangle(a, a, a, 60, 60, 60) {
-    _name = "Равносторонний треугольник";
-}
