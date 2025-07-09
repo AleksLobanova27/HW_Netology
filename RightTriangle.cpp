3eb2129b59aacda231c63322240c3408f2115763
AleksLobanova27 <elaginas27@gmail.com>
2025-07-09 15:38:50 +0300

    Delete RightTriangle.cpp

--- RightTriangle.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "RightTriangle.h"
-
-using namespace std;
-
-RightTriangle::RightTriangle(double a, double b, double c, double A, double B)
-    : Triangle(a, b, c, A, B, 90) {
-    _name = "Прямоугольный треугольник";
-}
