1a1f28db8b72d9378553f3524bd05d4452492187
Klea Helga <elaginas27@gmail.com>
2025-07-09 12:31:39 +0300

    commit 1

---
+++ Math.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+#include "Math.h"
+
+double MFunction::sum(double a, double b) { return a + b; }
+double MFunction::dif(double a, double b) { return a - b; }
+double MFunction::mul(double a, double b) { return a * b; }
+double MFunction::div(double a, double b) { return a / b; }
+double MFunction::deg(double a, double b) { return pow(a, b); }
---
+++ Математические функции.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include "Math.h"
+
+using namespace std;
+
+int main(){
+	setlocale(LC_ALL, "Russian");
+
+	int a;
+	int b;
+	int numOper;
+	cout << "Введите первое число : \n";
+	cin >> a;
+	cout << "Введите второе число : \n";
+	cin >> b;
+	cout << "Выберите операцию(1 - сложение, 2 вычитание, 3 - умножение, 4 - деление, 5 - возведение в степень) :\n";
+	cin >> numOper;
+
+	if (numOper == 1) { cout << a << " + " << b << " = " << MFunction::sum(a, b) << "\n"; }
+	if (numOper == 2) { cout << a << " - " << b << " = " << MFunction::dif(a, b) << "\n"; }
+	if (numOper == 3) { cout << a << " * " << b << " = " << MFunction::mul(a, b) << "\n"; }
+	if (numOper == 4) { cout << a << " : " << b << " = " << MFunction::div(a, b) << "\n"; }
+	if (numOper == 5) { cout << a << " в степени " << b << " = " << MFunction::deg(a, b) << "\n"; }
+	
+	return EXIT_SUCCESS;
+}
