6216171675894d49a082c489fee65634a9eb72a0
AleksLobanova27 <elaginas27@gmail.com>
2025-07-09 15:50:37 +0300

    Delete IsoTriangle.cpp

--- IsoTriangle.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "IsoTriangle.h"
-
-using namespace std;
-
-IsoTriangle::IsoTriangle(double a, double b, double A, double B)
-    : Triangle(a, b, a, A, B, A) {
-    _name = "Равнобедренный треугольник";
-}
