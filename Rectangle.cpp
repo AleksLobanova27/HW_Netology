10aa5a2c231a2010f4b72f7b71262c2bbdc38685
AleksLobanova27 <elaginas27@gmail.com>
2025-07-09 15:53:26 +0300

    Delete Rectangle.cpp

--- Rectangle.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Rectangle.h"
-
-using namespace std;
-
-Rectangle::Rectangle(double a, double b)
-    : Quadriateral(a, b, a, b, 90, 90, 90, 90) {
-    _name = "Прямоугольник";
-}
