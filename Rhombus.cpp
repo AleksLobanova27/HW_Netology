de88554117e551a3f0cf6310cf3363f9bdf6f124
AleksLobanova27 <elaginas27@gmail.com>
2025-07-09 15:54:32 +0300

    Delete Rhombus.cpp

--- Rhombus.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Rhombus.h"
-
-using namespace std;
-
-Rhombus::Rhombus(double a, double A, double B)
-    : Quadriateral(a, a, a, a, A, B, A, B) {
-    _name = "Ромб";
-}
